a7d0a65adecb54c89b774828cc1b1e14ecc07882
Roman Choporov <ChoporovRoman@gmail.com>
2024-02-05 08:48:15 +0100

    Added list of frames to the addon

--- AdvancedMacro.lua
+++ AdvancedMacro.lua
@@ -1,8 +1,28 @@
+-- Define paths to your icons
+local iconPaths = {
+    "Interface\\Icons\\Ability_Racial_Avatar",
+    "Interface\\Icons\\Ability_Racial_BloodRage",
+};
+local iconFrames = {};
+function MyAddon_CreateIconFrames(ParentFrame)
+    for i, path in ipairs(iconPaths) do
+        FRAME_SIZE=48
+        local frame = CreateFrame("Frame", "AdvancedMacroIconFrame" .. i, ParentFrame);
+        frame:SetSize(FRAME_SIZE, FRAME_SIZE);
+        frame:SetPoint("TOPLEFT", ParentFrame, (i-1)*FRAME_SIZE, -20);
+        local texture = frame:CreateTexture(nil, "BACKGROUND");
+        texture:SetAllPoints();
+        texture:SetTexture(path);
+        iconFrames[i] = frame;
+    end
+end
+
+
 local AdvancedMacro = {};
 
 local AdvancedMacroFrame = CreateFrame("Frame", "AdvancedMacroFrame", UIParent, "BasicFrameTemplate");
-AdvancedMacroFrame:SetSize(300, 200); -- Width and height
-AdvancedMacroFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0); -- Center the frame on the screen
+AdvancedMacroFrame:SetSize(400, 300);
+AdvancedMacroFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0);
 AdvancedMacroFrame.TitleText:SetText("Advanced Macro Window");
 AdvancedMacroFrame:Hide()
 
@@ -11,6 +31,9 @@ SLASH_ADVANCEDMACRO2 = "/advancedmacro"
 SlashCmdList["ADVANCEDMACRO"] = function(msg)
    AdvancedMacroFrame:Show()
 end
+
+MyAddon_CreateIconFrames(AdvancedMacroFrame);
+
 
 -- Assign your frame to your addon's namespace for future reference
 AdvancedMacro.Frame = AdvancedMacroFrame;
